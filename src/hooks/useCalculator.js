20d801bf7ee59eab1241584d1ceed3d066f33ecd
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2026-03-26 18:38:39 -0500

    prevent invalid input like consecutive operators (2++9)

--- src/hooks/useCalculator.js
+++ src/hooks/useCalculator.js
@@ -4,9 +4,16 @@ import { evaluate } from "../utils/evaluate";
 function useCalculator() {
     const [value, setValue] = useState("");
     const [history, setHistory] = useState([]);
+    const isOperator = (char) => ["+", "-", "*", "/", "%"].includes(char);
 
     const append = (char) => {
-        setValue((prev) => prev + char);
+        setValue((prev) => {
+            if (isValidAppend(prev, char)) {
+                return prev + char;
+            } else {
+                return prev;
+            }
+        });
     };
 
     const compute = () => {
@@ -22,6 +29,28 @@ function useCalculator() {
 
     const clear = () => {
         setValue("");
+    };
+
+    const isValidAppend = (current, nextChar) => {
+        const lastChar = current[current.length - 1];
+
+        if (nextChar === "." && current.match(/\d*\.\d*$/)) {
+            return false;
+        }
+
+        if (lastChar === "(" && nextChar === ")") {
+            return false;
+        }
+
+        if (isOperator(lastChar) && isOperator(nextChar)) {
+            return false;
+        }
+
+        if (lastChar === "(" && isOperator(nextChar) && nextChar !== "-") {
+            return false;
+        }
+
+        return true;
     };
 
     return { value, append, compute, history, clear };
